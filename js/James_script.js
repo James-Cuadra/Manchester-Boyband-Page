9ff8335a81917a769c0761bcd58f76bb2453f234
James Cuadra <62761473+James-Cuadra@users.noreply.github.com>
2022-12-16 13:09:32 +0000

    Update James_script.js

--- js/James_script.js
+++ js/James_script.js
@@ -4,10 +4,10 @@ function changePage() {
   document.documentElement.classList.toggle("js-modify-page");
   if(icon.className!="light")
     {
-        root.style.setProperty('--background-color', "rgb(252, 251, 231)")
-        root.style.setProperty('--text-color', "rgb(23, 22, 22)")
-        root.style.setProperty('--nav-color', "rgb(85, 85, 85)")
-        root.style.setProperty('--nav-text-color', "rgb(255, 255, 255)")
+        root.style.setProperty('--background-color', "rgb(0, 0, 0)")
+        root.style.setProperty('--text-color', "rgb(0, 0, 0)")
+        root.style.setProperty('--nav-color', "rgb(0, 0, 0)")
+        root.style.setProperty('--nav-text-color', "rgb(0, 0, 0)")
         icon.src='../img/light.png';
         icon.className="light";
     }
